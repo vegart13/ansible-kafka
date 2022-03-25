c0739d76f57e9f226ed4586b87d84d22561a328b
m92048 <>
2022-03-25 12:03:27 +0100

    required parameters added

--- lib/ansible/modules/kafkaesque.py
+++ lib/ansible/modules/kafkaesque.py
@@ -17,67 +17,65 @@ def main():
     # Set variables as global for cross-function accessability
     global module, create, delete, describe
 
-    # Define available arguments/parameters a user can pass to the module
-    # fields = {
-        # "custom_bin_path":                              {"type": "str", "default": "kafka-topics"},
-        # "create":                                       {"type": "bool", "default": False},
-        # "delete":                                       {"type": "bool", "default": False},
-        # "describe":                                     {"type": "bool", "default": False},
-        # "topic_name":                                   {"required": True, "type": "str"},
-        # "bootstrap_server":                             {"required": True, "type": "str"},
-        # "port":                                         {"required": True, "type": "int"},
-        # "command_config":                               {"type": "str"},
-        # "cleanup_policy":                               {"type": "str"},
-        # "confluent_key_schema_validation":              {"type": "str"},
-        # "confluent_key_subject_name_strategy":          {"type": "str"},
-        # "confluent_tier_enable":                        {"type": "str"},
-        # "confluent_tier_local_hotset_bytes":            {"type": "str"},
-        # "confluent_value_schema_validation":            {"type": "str"},
-        # "confluent_value_subject_name_strategy":        {"type": "str"},
-        # "delete_retention_ms":                          {"type": "str"},
-        # "file_delete_ms":                               {"type": "str"},
-        # "flush_ms":                                     {"type": "str"},
-        # "follower_replication_throttled_replicas":      {"type": "str"},
-        # "index_interval_bytes":                         {"type": "str"},
-        # "leader_replication_throttled_replicas":        {"type": "str"},
-        # "max_compaction_lag_ms":                        {"type": "str"},
-        # "max_message_bytes":                            {"type": "str"},
-        # "message_timestamp_type":                       {"type": "str"},
-        # "min_cleanable_dirty_ratio":                    {"type": "str"},
-        # "min_compaction_lag_ms":                        {"type": "str"},
-        # "min_insync_replicas":                          {"type": "str"},
-        # "preallocate":                                  {"type": "str"},
-        # "retention_bytes":                              {"type": "str"},
-        # "segment_bytes":                                {"type": "str"},
-        # "segment_index_bytes":                          {"type": "str"},
-        # "segment_jitter_ms":                            {"type": "str"},
-        # "segment_ms":                                   {"type": "str"},
-        # "unclean_leader_election_enable":               {"type": "str"},
-        # "confluent_placement_constraints":              {"type": "str"},
-        # "message_downconversion_enable":                {"type": "str"},
-    # }
-
     module_args = dict(
-        custom_bin_path=dict(type='str', default='kafka-topics'),
-        create=dict(type='bool', default=False),
-        delete=dict(type='bool', default=False),
-        describe=dict(type='bool', default=False),
-        topic_name=dict(type='str', required=True),
-        port=dict(type='str', required=True)
+        custom_bin_path                             =dict(type='str', default='kafka-topics'),
+        create                                      =dict(type='bool', default=False),
+        delete                                      =dict(type='bool', default=False),
+        describe                                    =dict(type='bool', default=False),
+        topic_name                                  =dict(type='str', required=True),
+        port                                        =dict(type='str', required=True),
+        bootstrap_server                            =dict(type='str', required=True),
+        command_config                              =dict(type='str'),
+        cleanup_policy                              =dict(type='str'),
+        confluent_key_schema_validation             =dict(type='str'),
+        confluent_key_subject_name_strategy         =dict(type='str'),
+        confluent_tier_enable                       =dict(type='str'),
+        confluent_tier_local_hotset_bytes           =dict(type='str'),
+        confluent_value_schema_validation           =dict(type='str'),
+        confluent_value_subject_name_strategy       =dict(type='str'),
+        delete_retention_ms                         =dict(type='str'),
+        file_delete_ms                              =dict(type='str'),
+        flush_ms                                    =dict(type='str'),
+        follower_replication_throttled_replicas     =dict(type='str'),
+        index_interval_bytes                        =dict(type='str'),
+        leader_replication_throttled_replicas       =dict(type='str'),
+        max_compaction_lag_ms                       =dict(type='str'),
+        max_message_bytes                           =dict(type='str'),
+        message_timestamp_type                      =dict(type='str'),
+        min_cleanable_dirty_ratio                   =dict(type='str'),
+        min_compaction_lag_ms                       =dict(type='str'),
+        min_insync_replicas                         =dict(type='str'),
+        preallocate                                 =dict(type='str'),
+        retention_bytes                             =dict(type='str'),
+        segment_bytes                               =dict(type='str'),
+        segment_index_bytes                         =dict(type='str'),
+        segment_jitter_ms                           =dict(type='str'),
+        segment_ms                                  =dict(type='str'),
+        unclean_leader_election_enable              =dict(type='str'),
+        confluent_placement_constraints             =dict(type='str'),
+        message_downconversion_enable               =dict(type='str')
     )
 
     # Store variables
-    module = AnsibleModule(argument_spec=module_args)
-    create = module.params['create']
-    delete = module.params['delete']
-    describe = module.params['describe']
+    module          = AnsibleModule(argument_spec=module_args)
+    create          = module.params['create']
+    delete          = module.params['delete']
+    describe        = module.params['describe']
 
     # if the user is working with this module in only check mode we do not
     # want to make any changes to the environment, just return the current
     # state with no modifications
     
+    # Validate that an action has been selected (Create, Delete og Describe), as well as validate that not more than one is selected
     validate_action_input()
     
+    # Create string containing kafka-topics path as well as flag related to action
+    create_action_string()
+    
+    # Add remaining required parameters
+    append_remaining_required_params()
+
+
     module.exit_json(changed=True, meta=module.params)
 
 def validate_action_input():
@@ -101,88 +99,25 @@ def validate_action_input():
         module.fail_json(msg='Create, describe or delete must be set True.')
     else: pass
 
+def create_action_string():
+    if create == True:
+        module.params.update({"action": module.params['custom_bin_path'] + " --create "})
+    elif delete == True:
+        module.params.update({"action": module.params['custom_bin_path'] + " --delete "})
+    elif describe == True:
+        module.params.update({"action": module.params['custom_bin_path'] + " --describe "})
+    else: module.fail_json(msg='Should not be reached')
+
+def append_remaining_required_params():
+
+    module.params.update({"action_with_required": \
+                            module.params['action'] + \
+                            module.params['bootstrap_server'] + \
+                            ':' + \
+                            module.params['port'] + ' --topic ' + \
+                            module.params['topic_name'] \
+    })
     
-    # validate_action_input()
-
-# module.exit_json(changed=True, meta=module.params)
-
-# def validate_action_input():
-#     # Verify that only one action has been chosen
-#     if str(create).lower == 'create' and str(delete).lower == str(delete).lower == 'delete':
-#         module.fail_json(msg='Action must be set to Create, Describe or Delete')
-#     else: module.fail_json(msg='Both create and delete cannot be True at the same time.')
-
-# def create_action_string():
-#     # Create string based on input
-#     if str(create).lower == 'create':
-#         module.params.update({"action": "~/Desktop/Kafka/bin/kafka-topics --create"})
-#     elif str(delete).lower == 'delete':
-#         module.params.update({"action": "~/Desktop/Kafka/bin/kafka-topics --delete"})
-#     elif str(describe).lower == 'describe':
-#         module.params.update({"action": "~/Desktop/Kafka/bin/kafka-topics --describe"})
-#     else: module.fail_json(msg='Action must be set to Create, Describe or Delete')
-
-
-# 3. Store string to variable and make accessible globally
-
-
-
-    # match create:
-    #     case True:
-    #         action_string = kafka.params["custom_bin_path"] + " --create"
-    #         return action_string
-    #     case False:
-    #         print("CREATE = FALSE")
-    #         return action_string
-
-    # match delete:
-    #     case True:
-    #         action_string = kafka.params["custom_bin_path"] + " --delete"
-    #         return action_string
-    #     case False:
-    #         print("DELETE = FALSE")
-
-    # match describe:
-    #     case True:
-    #         action_string = kafka.params["custom_bin_path"] + " --describe"
-    #         return action_string
-    #     case False:
-    #         print("DESCRIBE = FALSE")
-
-
-
-    ### REQUIRED PARAMETERS
-    # Match action to Create, Describe or Delete and update command part. Error returned if mismatch.
-    # if kafka.params["action"].lower() == "create":
-    #     kafka.params.update({"action": "~/Desktop/Kafka/bin/kafka-topics --create"})
-    # elif kafka.params["action"].lower() == "delete":
-    #     kafka.params.update({"action": "~/Desktop/Kafka/bin/kafka-topics --delete "})
-    # elif kafka.params["action"].lower() == "describe":
-    #     kafka.params.update({"action": "~/Desktop/Kafka/bin/kafka-topics --describe"})
-    # else: kafka.fail_json(msg='Action must be set to Create, Describe or Delete')
-
-    ### SWITCH TEST
-
-
-    # Add prefixes to command parameters (e.g.: --topic for topics, --bootstrap-server for bootstrap servers)
-    # kafka.params.update({"bootstrap_server": "--bootstrap-server " + kafka.params["bootstrap_server"] + ":" + str(kafka.params["port"])})
-    # kafka.params.update({"topic_name": "--topic " + kafka.params["topic_name"]})
-
-    # ### OPTIONAL REQUIREMENTS
-    # if kafka.params["command_config"] == '':
-    #     pass    
-    # else:
-    #     kafka.params.update({"command_config": "COMMAND CONFIG NOT FOUND"})
-
-    # if kafka.params["cleanup_policy"] == '':
-    #     pass
-    # else:
-    #     kafka.params.update({"cleanup_policy": "--config " + "cleanup.policy=" + kafka.params["cleanup_policy"]})
-
-    # Concatenate command based on updated variables
-    # required_string = kafka.params["action"] + " " + \
-    #                         kafka.params["bootstrap_server"] + " " + \
-    #                         kafka.params["topic_name"]
 
     # Save command to param then convert to string
     # kafka.params.update({"test": required_string})
